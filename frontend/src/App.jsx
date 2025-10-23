4dd2892729ffe6fbf1a1f0a54175f31d0f0db5b8
Dereje Tesfaye <deretesfa6@gmail.com>
2025-10-23 11:08:22 +0300

    react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+import { AdminDashbord } from "./Pages/AdminDashbord"
+import { Login } from "./Pages/Login"
+
 
 export const App = () => {
   return (
-    <div className="font-bold underline bg-amber-300">App</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<Navigate to="/admin-dashboard" />}></Route> */}
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/" element={<AdminDashbord />}></Route>
+
+        </Routes>
+
+      </BrowserRouter>
+    </div>
   )
 }
