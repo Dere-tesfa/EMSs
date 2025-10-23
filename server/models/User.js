6ba7bf2d9b718dabc43360535f3db7e52fb178a6
Dereje Tesfaye <deretesfa6@gmail.com>
2025-10-23 10:28:23 +0300

    change backend folder to server

---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+
+
+
+
+    },
+    password: {
+        type: String,
+        required: true,
+
+    },
+    role: {
+        type: String,
+        enum: ["Admin", "Employee"],
+        required: true,
+
+    },
+    profileImage: { type: String },
+
+    createAt: { type: Date, default: Date.now },
+    updateAt: { type: Date, default: Date.now },
+
+})
+const users = mongoose.model("users", userSchema);
+export default users;
