24f92985d6e7827575df3d0664d72b49862c33e9
Dereje Tesfaye <deretesfa6@gmail.com>
2025-10-28 16:49:14 +0300

    App file frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-import { AdminDashbord } from "./Pages/AdminDashbord"
+import { AdminDashbord } from "./Pages/AdminDashbord";
 import Login from "./Pages/Login";
-
-
+import { Register } from "./Pages/Register";
 
 export const App = () => {
   return (
@@ -13,11 +11,10 @@ export const App = () => {
         <Routes>
           {/* <Route path="/" element={<Navigate to="/admin-dashboard" />}></Route> */}
           <Route path="/login" element={<Login />}></Route>
-          <Route path="/" element={<AdminDashbord />}></Route>
-
+          <Route path="/dashboard" element={<AdminDashbord />}></Route>
+          <Route path="/register" element={<Register />}></Route>
         </Routes>
-
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
