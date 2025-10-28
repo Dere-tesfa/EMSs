469ea3aa53ed4bbf12686b4c30d3950898fa1a21
Dereje Tesfaye <deretesfa6@gmail.com>
2025-10-28 16:50:56 +0300

    login schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,33 +1,23 @@
+// import mongoose from "mongoose";
+
+// const userSchema = new mongoose.Schema({
+//   // name: { type: String, required: true },
+//   email: { type: String, required: true },
+//   password: { type: String, required: true },
+//   // role: { type: String, enum: ["admin", "employee"], required: true },
+//   // profileImage: { type: String },
+//   // createAt: { type: Date, default: Date.now },
+//   // updateAt: { type: Date, default: Date.now },
+// });
+
+// export default mongoose.model("User", userSchema);
+
 import mongoose from "mongoose";
+
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
 
-
-
-
-    },
-    password: {
-        type: String,
-        required: true,
-
-    },
-    role: {
-        type: String,
-        enum: ["Admin", "Employee"],
-        required: true,
-
-    },
-    profileImage: { type: String },
-
-    createAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
-
-})
-const users = mongoose.model("users", userSchema);
-export default users;
+export default mongoose.model("User", userSchema);
